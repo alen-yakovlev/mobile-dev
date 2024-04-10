236b8da00aa14e344b8fd127b7a14a0aac0033b5
Alen Yakovlev <azy0030@auburn.edu>
2024-04-09 23:49:25 -0700

    Initial Commit

---
+++ Golf Scores App/Golf_Scores_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Golf_Scores_AppApp.swift
+//  Golf Scores App
+//
+//  Created by Alen Yakovlev on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Golf_Scores_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
